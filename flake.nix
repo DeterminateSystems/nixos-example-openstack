155cfe36b2836d5baf90078e949e1ab2fec0a455
Graham Christensen <graham@grahamc.com>
2023-10-10 16:15:02 -0400

    Mechanical migration to FlakeHub

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS + Openstack tooling";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.533189.tar.gz";
   };
 
   outputs =
